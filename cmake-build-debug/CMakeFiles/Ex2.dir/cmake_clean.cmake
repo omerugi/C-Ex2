1690624ac3fc16d4ef1ac5969155e66f4c2e811f
omerugi <omerihay@gmail.com>
2019-12-13 16:38:36 +0200

    init3

---
+++ cmake-build-debug/CMakeFiles/Ex2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex2.dir/isort.c.o"
+  "CMakeFiles/Ex2.dir/main.c.o"
+  "CMakeFiles/Ex2.dir/txtfind.c.o"
+  "Ex2"
+  "Ex2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
